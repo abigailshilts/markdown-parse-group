217daedc6cda1c25044adc3971ca3d2ebee3230f
abigailshilts <abbi@shilts.net>
2022-02-02 15:40:56 -0800

    fixed the test again???

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,7 +14,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse-group/test-file.md"));
+        String contents= Files.readString(Path.of("./test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
